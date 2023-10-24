ba53470b6275f2e26e3f3ce54eba7d4146046dd7
JungGyu00 <co399955@gmail.com>
2023-10-24 10:58:35 +0900

    feat: add user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,77 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from user.models import User
 
-# Register your models here.
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ["username",]
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ["username", "nickname", "email",
+                  "password", "is_active", "is_admin"]
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["id", "username", "email", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["username", "nickname", "email", "password"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["username", "nickname", "email", "password1", "password2", "image"],
+            },
+        ),
+    ]
+    search_fields = ["username"]
+    ordering = ["username"]
+    filter_horizontal = []
+
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
