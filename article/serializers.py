257747c1ea86b532f4c5aaea00e1921960aed1d1
solleepang <dlthf727@gmail.com>
2023-10-26 03:40:35 +0900

    fix: article fix

--- article/serializers.py
+++ article/serializers.py
@@ -6,6 +6,8 @@ from user.models import User
 
 class ArticleSerializer(serializers.ModelSerializer):
     member_count = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = Article
         fields = '__all__'
@@ -14,3 +16,8 @@ class ArticleSerializer(serializers.ModelSerializer):
         """ 해당 게시글에 등록된 밥친구 몇명인지 반환 """
         member_count = len(User.objects.filter(user_friend=obj.id))
         return member_count
+
+    def get_username(self, obj):
+        """ 작성자의 유저이름 반환 """
+        user = User.objects.get(id=obj.author_id.id)
+        return user.username
